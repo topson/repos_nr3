ad3361fc3e5b6a78ad5343d7d4523517d5569620
tbi tbi <tbi@tbi.de>
2015-10-06 16:54:30 +0200

    added a new class checkbot to improve checkmove flexibility, minor changes in the layout

---
+++ app/src/main/java/com/example/tschumacher/myfirstapp/CheckBot.java
@@ -0,0 +1,79 @@
+package com.example.tschumacher.myfirstapp;
+
+/**
+ * Created by t.schumacher on 06.10.2015.
+ */
+
+public class CheckBot {
+    int gameOver;
+    protected int checkRight ( String board [][] ){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                try {
+                    if ( !board[i][j].equals("-") && board[i][j].equals(board[i][j+1]) && board[i][j].equals(board[i][j+2]) ){
+                        gameOver = 1;
+                        return gameOver;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    gameOver = 0;
+                    return gameOver;
+                }
+            }
+        }
+        return gameOver;
+    }
+
+    protected int checkDown( String board[][] ) {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                try {
+                    if ( !board[i][j].equals("-") && board[i][j].equals(board[i+1][j]) && board[i][j].equals(board[i+2][j]) ){
+                        gameOver = 1;
+                        return gameOver;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    gameOver = 0;
+                    return gameOver;
+                }
+            }
+        }
+        return gameOver;
+    }
+
+    protected int checkDR ( String board [][]){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                try {
+                    if ( !board[i][j].equals("-") && board[i][j].equals(board[i+1][j+1]) && board[i][j].equals(board[i+2][j+2]) ){
+                        gameOver = 1;
+                        return gameOver;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    gameOver = 0;
+                    return gameOver;
+                }
+            }
+        }
+        return gameOver;
+    }
+
+    protected int checkUR ( String board [][] ){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                try {
+                    if ( !board[i][j].equals("-") && board[i][j].equals(board[i - 1][j + 1]) && board[i][j].equals(board[i - 2][j + 2])) {
+                        gameOver = 1;
+                        return gameOver;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                       gameOver = 0;
+                }
+            }
+        }
+        return gameOver;
+    }
+}
--- app/src/main/java/com/example/tschumacher/myfirstapp/CheckMove.java
+++ app/src/main/java/com/example/tschumacher/myfirstapp/CheckMove.java
@@ -4,31 +4,30 @@ package com.example.tschumacher.myfirstapp;
  * Created by t.schumacher on 05.10.2015.
  */
 public class CheckMove {
-
+    /*
     protected int checkRight ( int column ){
         int rightIndex = column + 1;
         return rightIndex;
     }
+
     protected int checkDown( int row ){
         int downIndex = row + 1;
         return downIndex;
     }
-    protected int checkLeft ( int column ){
-        int leftColumn = column - 1;
-        return leftColumn;
-    }
+
     protected int checkUp ( int row ){
         int upRow = row - 1;
         return upRow;
     }
-    protected int checking( String board [][], int player ){
+
+    protected int checking( String board [][], int player ) {
 
         int k;
         int m;
         int gameOver = 0;
         int winner;
-        for ( int i = 0; i < 3; i++ ){
-            for ( int j = 0; j < 3; j++ ) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 if (!board[i][j].equals("-")) {
                     if (0 == i && 0 == j) {
                         if (board[i][j].equals(board[checkDown(i)][checkRight(j)])) {
@@ -37,53 +36,60 @@ public class CheckMove {
                             if (board[k][m].equals(board[checkDown(k)][checkRight(m)])) {
                                 gameOver = 1;
                             }
-                        }
-                        else if (board[i][j].equals(board[checkDown(i)][j]) ){
+                        } else if (board[i][j].equals(board[checkDown(i)][j])) {
                             k = checkDown(i);
-                            if (board[k][j].equals(board[checkDown(k)][j]) ){
+                            if (board[k][j].equals(board[checkDown(k)][j])) {
                                 gameOver = 1;
                             }
-                        }
-                        else if (board[i][j].equals(board[i][checkRight(j)]) ){
+                        } else if (board[i][j].equals(board[i][checkRight(j)])) {
                             k = checkRight(j);
-                            if (board[i][k].equals(board[i][checkRight(k)]) ){
+                            if (board[i][k].equals(board[i][checkRight(k)])) {
                                 gameOver = 1;
                             }
                         }
-                    }
-                    else if (0 == j && 2 == i) {
+                    } else if (0 == j && 2 == i) {
                         if (board[i][j].equals(board[checkUp(i)][checkRight(j)])) {
                             k = checkUp(i);
                             m = checkRight(j);
                             if (board[k][m].equals(board[checkUp(k)][checkRight(m)])) {
                                 gameOver = 1;
                             }
-                        }
-                        else if (board[i][j].equals(board[i][checkRight(j)]) ){
+                        } else if (board[i][j].equals(board[i][checkRight(j)])) {
                             k = checkRight(j);
-                            if (board[i][k].equals(board[i][checkRight(k)]) ){
+                            if (board[i][k].equals(board[i][checkRight(k)])) {
                                 gameOver = 1;
                             }
                         }
-                    }
-                    else if ( 0 == i ){
-                        if (board[i][j].equals(board[checkDown(i)][j]) ){
+                    } else if (0 == i) {
+                        if (board[i][j].equals(board[checkDown(i)][j])) {
                             k = checkDown(i);
-                            if (board[k][j].equals(board[checkDown(k)][j]) ){
+                            if (board[k][j].equals(board[checkDown(k)][j])) {
                                 gameOver = 1;
                             }
                         }
-                    }
-                    else if ( 0 == j ){
-                        if (board[i][j].equals(board[i][checkRight(j)]) ){
+                    } else if (0 == j) {
+                        if (board[i][j].equals(board[i][checkRight(j)])) {
                             k = checkRight(j);
-                            if (board[i][k].equals(board[i][checkRight(k)]) ){
+                            if (board[i][k].equals(board[i][checkRight(k)])) {
                                 gameOver = 1;
                             }
                         }
                     }
                 }
             }
+        }
+    }*/
+
+    protected int checking( String board [][], int player ) {
+        int gameOver;
+        int winner;
+
+        CheckBot checkBot = new CheckBot();
+        if (checkBot.checkRight(board) == 1 || checkBot.checkDown(board) == 1 || checkBot.checkDR(board) == 1 || checkBot.checkUR(board) == 1) {
+            gameOver = 1;
+        }
+        else {
+            gameOver = 0;
         }
         if ( 1 == gameOver && 1 == player){
             winner = 1;
